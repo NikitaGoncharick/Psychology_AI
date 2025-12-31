0e89619fe3ecd28a7a3b85fe963ef98a64e8b467
NikitaGoncharick <93119559+NikitaGoncharick@users.noreply.github.com>
2025-12-31 22:38:27 +0100

    Commit

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 # main.py
 import sys
 import os
-sys.path.insert(0, os.path.dirname(__file__))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from contextlib import asynccontextmanager
 from redis.asyncio import Redis, RedisError
